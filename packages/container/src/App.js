57e4ee2ef81c0ffb98411f2cc8649e07fe915f32
vlad.ocheretnyi322 <vlad.ocheretnyi322@gmail.com>
2024-09-28 15:23:51 +0300

    task: fix style collision

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,22 @@
+import {createGenerateClassName, StylesProvider} from "@material-ui/core";
+import {BrowserRouter} from "react-router-dom";
+import {Header} from "./components/Header";
 import {MarketingApp} from "./components/MarketingApp";
 import React from 'react';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export const App = () => {
-  return <>
-    <h1>hello there</h1>
-    <br/>
-    <MarketingApp />
-  </>
+  return (
+      <BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+          <div>
+            <Header/>
+            <MarketingApp />
+          </div>
+        </StylesProvider>
+      </BrowserRouter>
+  )
 }
